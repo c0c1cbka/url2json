a55e4fb32d08b178cf8c796f1b28fe776e614512
APVeremenko <APVeremenko@greenatom.ru>
2022-07-12 10:51:25 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+var qs = require('qs');
+var ncp = require('copy-paste');
+
+function replaceStrings(obj){
+    if (obj) {
+        for (const key in obj) {
+            if (obj.hasOwnProperty(key) ) {
+                if (typeof obj[key] === 'object' && obj[key] !== null) {
+                        obj[key] = replaceStrings(obj[key]);
+                    }
+                if (Array.isArray(obj[key])) {
+                    obj[key] = replaceStrings(obj[key]);
+                }
+                const result = parseInt(obj[key], 10);
+                if (!isNaN(result) && result.toString(10).length === (obj[key]).length) {
+                    obj[key] = result;
+                } else {
+                    if (obj[key] === 'true') {
+                        obj[key] = true;
+                    } else if (obj[key] === 'false') {
+                        obj[key] = false;
+                    } else if (obj[key] === '') {
+                        obj[key] = null;
+                    }
+                }
+            }
+        }
+    }
+    return obj;
+}
+
+let urlString = process.argv[2];
+let paramsIndex = urlString.indexOf('?', 0);
+if (paramsIndex !== -1) {
+    urlString = urlString.slice(paramsIndex+1);
+}
+
+let resultParse = qs.parse(urlString);
+let resultConvert = replaceStrings(resultParse)
+let resultString = JSON.stringify(resultConvert);
+
+ncp.copy(resultString)
+console.log('\n\n'+ resultString);
+
